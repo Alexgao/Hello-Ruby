06ac9707f6f9fdcfca39cb367e913d2a3ff194ae
Alex Gao <1559715597@qq.com>
2013-08-23 17:10:47 +0800

    Alex

--- RubyLanguageLearn/helloworld.rb
+++ RubyLanguageLearn/helloworld.rb
@@ -74,17 +74,191 @@
 #    my_age, my_height, my_weight, my_age + my_height + my_weight]
 
 
+#####################################
+
+#class Person
+#  attr_accessor :name
+
+#  def set_name(your_name)
+#    @name = your_name
+#  end
+#end
+
+#m = Person.new
+#p m.name
+#m.set_name('today')
+#p m.name # => 猜是什么
+
+#####################################
+
+#test = "%s %s %s %s"
+
+#puts test % [1,2,3,4]
+#puts test % ["one","two","three","four"]
+
+#####################################
 
 
-class Person
-  attr_accessor :name
 
-  def set_name(your_name)
-    @name = your_name
-  end
-end
+#days = "Mon Tue Wed Thu Fri Sat Sun"
+#months = "Jan\nFeb\nApr\nMay\nJun\nJil\nAug"
 
-m = Person.new
-p m.name
-m.set_name('today')
-p m.name # => 猜是什么
+#puts "days:", days
+#puts "months:", months
+
+#one = <<PARAGRAPH
+#There's something going on here.
+#With the three double-quotes.
+#We'll be able to type as much as we like.
+#Even 4 lines if we want, or 5, or 6.
+#PARAGRAPH
+
+#puts one
+
+################转译符#####################
+
+
+#tabby_cat = "\tI'm tabbed in."
+#persian_cat = "I'm split\non a line."
+#backslash_cat = "I'm \\ a \\ cat."
+
+#fat_cat = <<MY_HEREDOC
+#I'll do a list:
+#\t* Cat food
+#\t* Fishies
+#\t* Catnip\n\t* Grass
+#MY_HEREDOC
+
+#puts tabby_cat
+#puts persian_cat
+#puts backslash_cat
+#puts fat_cat
+
+#####################################
+
+#print "How old are you?"
+#age = gets.chomp()
+#print "How tall are you?"
+#height = gets.chomp()
+#print "How much do you weigh?"
+#weight = gets.chomp()
+
+#puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+#####################################
+
+#require 'open-uri'
+
+#open("http://www.ruby-lang.org/en") do |f|
+#	f.each_line {|line| p line}
+#	puts f.base_uri
+#	puts f.content_type
+#	puts f.charset
+#	puts f.content_encoding
+#	puts f.last_modified
+#end
+
+
+#Require：
+#require方法让你加载一个库，并且只加载一次，如果你多次加载会返回false。只有当你要加载的库位于一个分离的文件中时才有必要使用require。使用时不需要加扩展名，一般放在文件的最前面：
+#    require ‘test_library’
+#Load：
+#load用来多次加载一个库，你必须指定扩展名：
+#   load ‘test_library.rb’
+#Include:
+#当你的库加载之后，你可以在你的类定义中包含一个module，让module的实例方法和变量成为类本身的实例方法和类变量，它们mix进来了。根据锄头书，include并不会把module的实例方法拷贝到类中，只是做了引用，包含module的不同类都指向了同一个对象。如果你改变了module的定义，即使你的程序还在运行，所有包含module的类都会改变行为。
+#   module Log
+#      def class_type
+#           “This class is of type: #{self.class}”
+ #      end
+ #  end
+#   class TestClass
+#       include Log
+#   end
+#   tc=TestClass.new.class_type    #=>This class is of type: TestClass
+#Extend:
+#extend会把module的实例方法作为类方法加入类中：
+#   module Log
+#       def class_type
+#           “This class is of type: #{self.class}”
+#       end
+#   end
+ #  class TestClass
+ #      extend Log
+ #  end
+ #  tc=TestClass.class_type       #=>This class is of type: TestClass 
+
+
+
+#########################################
+
+#first, second, third = ARGV
+
+#puts "The script is called: #{$0}"
+#puts "Your first variable is: #{first}"
+#puts "Your second variable is: #{second}"
+#puts "Your third variable is: #{third}"
+
+
+#**执行语句：ruby ex13.rb first 2nd 3rd
+
+
+
+
+#########################################
+
+user = ARGV.first
+prompt = '>'
+
+puts "Hi #{user}, I'm the #{$0} script."
+puts "I'd like to ask you a few questions."
+
+puts "Do you like me #{user}?"
+print prompt
+likes = STDIN.gets.chomp()
+
+puts "Where do you live #{user}?"
+print prompt
+lives = STDIN.gets.chomp()
+
+puts "What kind of computer do you have?"
+print prompt
+computer = STDIN.gets.chomp()
+
+puts <<MESSAGE
+Alright, so you said #{likes} about liking me.
+You live in #{lives}.  Not sure where that is.
+And you have a #{computer} computer.  Nice.
+MESSAGE
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
